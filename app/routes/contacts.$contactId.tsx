e53acfda8c346feaa00361b4b1becfb37065afd7
Miroslava Hreskova <hroncovam@yahoo.com>
2024-06-12 14:41:20 +0100

    Improving readability

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -9,10 +9,13 @@ import { getContact } from "../data";
 
 export const loader = async ({ params, }: LoaderFunctionArgs) => {
     invariant(params.contactId, "Missing contactId param");
+
     const contact = await getContact(params.contactId);
+
     if (!contact) {
       throw new Response("Not Found", { status: 404 });
     }
+    
     return json({ contact });
   };
 
