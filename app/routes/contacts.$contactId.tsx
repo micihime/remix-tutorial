ecc8537e3d3c564c246196f5af177917c794825b
Miroslava Hreskova <hroncovam@yahoo.com>
2024-06-13 11:02:25 +0100

    Adding to favorites
    - in order to submit a form without causing a navigation, there is useFetcher which allows us to communicate with actions and loaders without causing a navigation
    - the ★ button on the contact page makes sense for this. We aren't creating or deleting a new record, and we don't want to change pages. We simply want to change the data on the page we're looking at.

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,23 +1,29 @@
-import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, json, LoaderFunctionArgs } from "@remix-run/node";
+import { Form, useFetcher, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
 
 import invariant from "tiny-invariant";
 
 import type { ContactRecord } from "../data";
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
 
 export const loader = async ({ params, }: LoaderFunctionArgs) => {
-    invariant(params.contactId, "Missing contactId param");
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  
+  return json({ contact });
+};
 
-    const contact = await getContact(params.contactId);
-
-    if (!contact) {
-      throw new Response("Not Found", { status: 404 });
-    }
-    
-    return json({ contact });
-  };
+export const action = async ({ params, request, }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+};
 
 export default function Contact() {
   const contactt : any = useLoaderData<typeof loader>();
@@ -89,10 +95,11 @@ export default function Contact() {
 const Favorite: FunctionComponent<{
   contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
+  const fetcher = useFetcher();
   const favorite = contact.favorite;
 
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         aria-label={
           favorite
@@ -104,6 +111,6 @@ const Favorite: FunctionComponent<{
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 };
